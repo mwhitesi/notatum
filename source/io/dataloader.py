e8ee8aa61f88efc8d0834354fd1e335b39835715
Matthew Whiteside <Matthew.Whiteside@albertahealthservices.ca>
2019-07-04 10:13:44 -0600

    ADD: data loading objects

--- source/io/dataloader.py
+++ source/io/dataloader.py
@@ -2,16 +2,67 @@ import pandas as pd
 import numpy as np
 import re
 import torch
+import torch.utils.data as data_utils
 
-# class GeneDataset(gluon.data.ArrayDataset):
-#
-#     def __init__(self, gene_filepath, meta_filepath):
-#
-#         # Load inputs
-#         X = 0
-#         y = 0
-#         # Initialize ArrayDataset object
-#         super().__init__(X, y)
+class IndexTensorDataset:
+    """
+
+        Identical to torch.utils.data.Dataset.TensorDataset, but __getitem__
+        also returns indices as last value in tuple
+
+    """
+
+    def __init__(self, *tensors):
+        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors)
+        self.tensors = tensors
+
+    def __getitem__(self, index):
+        #t = tuple([tensor[index] for tensor in self.tensors].append(index))
+        t = [tensor[index] for tensor in self.tensors]
+        t.append(index)
+        return(tuple(t))
+
+    def __len__(self):
+        return self.tensors[0].size(0)
+
+
+class GeneDataset:
+    """
+
+        Container object that provides access to the PyTorch Dataset and
+        Dataloader objects needed for one experiment
+
+    """
+
+    def __init__(self, data_file, batch_size, test_split, shuffle_dataset,
+                 random_seed):
+
+        # Load tensor data
+        data = torch.load(data_file)
+        dataset = IndexTensorDataset(data['X'], data['y'])
+
+        # Test / train split
+        dataset_size = len(dataset)
+        indices = list(range(dataset_size))
+        split = int(np.floor(test_split * dataset_size))
+        if shuffle_dataset:
+            np.random.seed(random_seed)
+            np.random.shuffle(indices)
+        train_indices, test_indices = indices[split:], indices[:split]
+
+        # Initialize Dataloaders
+        train_sampler = data_utils.SubsetRandomSampler(train_indices)
+        test_sampler = data_utils.SubsetRandomSampler(test_indices)
+
+        self.train_loader = data_utils.DataLoader(dataset,
+                                                  batch_size=batch_size,
+                                                  sampler=train_sampler)
+        self.test_loader = data_utils.DataLoader(dataset,
+                                                 batch_size=batch_size,
+                                                 sampler=test_sampler)
+        self.isolates = data['isolates']
+
+
 
 def transform(input, output):
     """Snakemake function
--- source/io/transform.py
+++
@@ -1,9 +0,0 @@
-from dataloader import transform
-
-def main():
-    transform(None, None, None)
-    print('ye')
-
-
-if __name__ == "__main__":
-    main()
